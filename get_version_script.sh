94152e2a2bcf9eed591c1b1c6f5c7294d764a446
Abhishek Andhare <andhareabhishek@gmail.com>
2020-04-24 09:37:07 -0700

    Update get_version_script.sh

--- get_version_script.sh
+++ get_version_script.sh
@@ -1,2 +1,3 @@
 echo Site Name=$(cat /data/ncconfig.txt | grep "\"Class\":\"LOCATION\"" | cut -d'"' -f12)
 cat /data/viewpackage
+cat /data/ncconfig.txt | grep "\"Class\":\"LOCATION\"" | cut -d'"' -f8
